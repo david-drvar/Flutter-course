1a4ebef0c906359cc3719d87a0fc1a01aca07a91
David <david@drvar@ccfr.cz>
2022-02-21 14:44:04 +0100

    quiz app initialization and basic styling

---
+++ quiz_app/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'question.dart';
+import 'answer.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+
+  var questions = [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': ['Black', 'Red', 'Green', 'White']
+    },
+    {
+      'questionText': 'What\'s your favorite animal',
+      'answers': ['Cat', 'Dog', 'Rabbit', 'Lion']
+    },
+    {
+      'questionText': 'What\'s your favorite instructor',
+      'answers': ['Max', 'Max', 'Max', 'Max']
+    },
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(children: [
+          Question(questions[_questionIndex]['questionText'] as String),
+          ...(questions[_questionIndex]['answers'] as List<String>)
+              .map((answer) {
+            return Answer(_answerQuestion, answer);
+          }).toList()
+        ]),
+      ),
+    );
+  }
+}
